00c1c38e203b2afa6b059cf82bcc3a29dba67ac7
hls <hilliassmirali@gmail.com>
2024-12-10 17:24:17 +0100

    add prototype of functions in the file 16...

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
-int binary_tree_is_perfect(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+
 /**
  * binary_tree_is_perfect - checks if the binary tree is perfect
  * @tree: a pointer to the root of the tree
