3cb73dc8576db05a46f111a86a6a6d451f81735a
hls <hilliassmirali@gmail.com>
2024-12-10 12:06:58 +0100

    binary_tree_node function created

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child
+ * @tree: const binary_tree_t *
+ * Return: size_t
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t has_child = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+		has_child = 1;
+	else
+		has_child = 0;
+
+	return (has_child + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
