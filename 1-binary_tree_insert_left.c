d352a641abc9a161c9334e0205f67822ce5a932c
Mame-Penda <mamependasadio1@gmail.com>
2024-12-10 17:10:05 +0100

    fixed betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert - inserts a node as the left-child of another node
+ * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: binary_tree_t *
  * @value: int
  * Return: binary_tree_t *
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *parent_node = parent;
 
 	if (!parent_node)
-		return( NULL);
+		return (NULL);
 
 	new_left_child = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (!new_left_child)
@@ -32,7 +32,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		parent_node->left->parent = new_left_child;
 		new_left_child->left = parent_node->left;
-		new_left_child->parent = parent_node; 
+		new_left_child->parent = parent_node;
 		parent_node->left = new_left_child;
 	}
 
