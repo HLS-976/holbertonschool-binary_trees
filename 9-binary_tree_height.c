1d57bd6d178a6a70ad9059b89ee0ea5a1b46975b
hls <hilliassmirali@gmail.com>
2024-12-10 17:17:34 +0100

    changed the position of the increment in the function binary_tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,16 +12,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+
 	if (tree->left)
 	{
-		count++;
 		binary_tree_height(tree->left);
+		count++;
 	}
 
 	if (tree->right)
 	{
-		count++;
 		binary_tree_height(tree->right);
+		count++;
 	}
 
 	return (count);
